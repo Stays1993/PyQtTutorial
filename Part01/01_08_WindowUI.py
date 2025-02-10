65a581c90a46f6166025fd91f1c1cc762187765e
Stays1993 <2548171533@qq.com>
2025-02-10 14:57:23 +0800

    refactor: 重命名 UI 文件和代码引用

--- Part01/01_08_WindowUI.py
+++ Part01/01_08_WindowUI.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file '1.8_WindowUI.ui'
+# Form implementation generated from reading ui file '01_08_WindowUI.ui'
 #
 # Created by: PyQt6 UI code generator 6.8.0
 #
