a63eed4b15f0146ebc092d599b8fccb462527be4
yujiuhuan <2548171533@qq.com>
2025-02-27 23:26:40 +0800

    02_19 学习QLCDNumber的使用

---
+++ Part02/02_19_LCDnumberex.py
@@ -0,0 +1,46 @@
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QLCDNumber,
+)
+from PyQt6.QtCore import QTime, QTimer
+
+
+class Window(QWidget):  # 定义Window类，继承自QWidget
+    def __init__(self):  # 定义构造函数
+        super().__init__()  # 调用父类构造函数
+        # 设置窗口标题
+        self.setWindowTitle("Python QLCDNumber")  # 设置窗体标题
+        # 设置窗口图标
+        self.setWindowIcon(QIcon("images/python.png"))  # 设置窗体图标
+        # 设置窗口大小及位置
+        self.setGeometry(300, 300, 700, 400)
+
+        timer = QTimer()
+        timer.timeout.connect(self.show_lcd)
+        timer.start(1000)
+
+        self.show_lcd()
+
+    def show_lcd(self):
+        vbox = QVBoxLayout()
+        lcd = QLCDNumber()
+        lcd.setStyleSheet("background:red")
+        vbox.addWidget(lcd)
+        time = QTime.currentTime()
+        text = time.toString("hh:mm")
+
+        lcd.display(text)
+
+        self.setLayout(vbox)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)  # 创建QApplication对象
+    window = Window()  # 创建Window对象
+    window.show()  # 显示窗体
+    sys.exit(app.exec())  # 进入应用程序的主循环，并等待退出
--- README.md
+++ README.md
@@ -6,31 +6,28 @@
 
 ## 2. 版本相关
 Python版本：`3.12.6`   
-包管理器：[`uv`](https://juejin.cn/post/7444548883646709796)
-
-`从项目配置安装`
+包管理器：[`uv`](https://juejin.cn/post/7444548883646709796)  
 依赖包：  
 1. black==25.1.0
 2. pyqt6==6.8.1
 3. pyside6>=6.8.2.1
 
+### uv官方资源
+- [GitHub仓库](https://github.com/astral-sh/uv)  
+- [中文教程](https://vra.github.io/2024/03/31/uv-tutorial1/)
+
 ## PyCharm 外部工具
-1. Qt Designer - 图形化QT设计软件
+### Qt Designer - 图形化QT设计软件
 - 名称：`Qt Designer`
 - 程序：`$PyInterpreterDirectory$\pyside6-designer.exe`
 - 工作目录：`$FileDir$`
+
 ![QT_Designer](./images/01_QT_Designer.png)
-2. PyUIC6 - 将ui文件转换为py文件
+
+### PyUIC6 - 将ui文件转换为py文件
 - 名称：`PyUIC6`
 - 程序：`$PyInterpreterDirectory$\pyuic6.exe`
 - 实参：`-x $FileName$ -o $FileNameWithoutAllExtensions$.py`
 - 工作目录：`$FileDir$`
+
 ![PyUIC6](./images/02_PyUIC6.png)
-
-
-
-
-
-
-
-
