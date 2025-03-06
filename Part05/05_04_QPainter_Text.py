b1ff3cb2c56f8ca92cc6622f47a40afc8866951c
Stays1993 <2548171533@qq.com>
2025-03-06 14:45:16 +0800

    05_04: 绘制文本 05_05: 绘制线性渐变颜色

---
+++ Part05/05_04_QPainter_Text.py
@@ -0,0 +1,39 @@
+from PyQt6.QtGui import QIcon, QPainter, QTextDocument
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import QRect, Qt, QRectF
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(400, 400, 500, 300)
+        self.setWindowTitle("Python 05_04_QPainter_Text")
+        self.setWindowIcon(QIcon("images/python.png"))
+
+    def paintEvent(self, event):
+        painter: QPainter = QPainter(self)
+
+        painter.drawText(50, 50, "PyQt6 Course")
+
+        rect: QRect = QRect(100, 150, 250, 25)
+        painter.drawRect(rect)
+        painter.drawText(rect, Qt.AlignmentFlag.AlignCenter, "Hello World !")
+
+        document: QTextDocument = QTextDocument()
+        rect_f: QRectF = QRectF(0, 0, 250, 250)
+        document.setTextWidth(rect_f.width())
+        document.setHtml(
+            "<b>Welcome to PyQt6 Course</b>"
+            "<i>Hello Python !</i>"
+            "<font size='15' color='red'>Welcome to Python</font>"
+        )
+        document.drawContents(painter, rect_f)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
---
+++ Part05/05_05_QLinearGradient.py
@@ -0,0 +1,34 @@
+from PyQt6.QtGui import QIcon, QLinearGradient, QPainter, QPen, QBrush
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import Qt
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(200, 200, 700, 500)
+        self.setWindowTitle("Python 05_05_QLinearGradient")
+        self.setWindowIcon(QIcon("images/python.png"))
+
+    def paintEvent(self, event):
+        painter: QPainter = QPainter(self)
+        painter.setPen(QPen(Qt.GlobalColor.black, 4, Qt.PenStyle.SolidLine))
+
+        grad_1: QLinearGradient = QLinearGradient(25, 100, 150, 175)
+
+        grad_1.setColorAt(0.0, Qt.GlobalColor.red)
+        grad_1.setColorAt(0.5, Qt.GlobalColor.green)
+        grad_1.setColorAt(1.0, Qt.GlobalColor.yellow)
+
+        painter.setBrush(QBrush(grad_1))
+
+        painter.drawRect(10, 10, 200, 200)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
