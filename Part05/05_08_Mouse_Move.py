00c5fa826cfa66c49cdeaaf046112121309984da
Stays1993 <2548171533@qq.com>
2025-03-06 16:29:12 +0800

    05_08: 鼠标移动轨迹

---
+++ Part05/05_08_Mouse_Move.py
@@ -0,0 +1,39 @@
+from PyQt6.QtGui import QIcon, QFont
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(200, 200, 700, 500)
+        self.setWindowTitle("Python 05_08_Mouse_Move")
+        self.setWindowIcon(QIcon("images/python.png"))
+
+        self.setMouseTracking(True)
+
+        hbox: QHBoxLayout = QHBoxLayout()
+
+        self.label: QLabel = QLabel("鼠标轨迹")
+        self.label.setFont(QFont("Times", 15, 800))
+
+        hbox.addWidget(self.label)
+
+        self.setLayout(hbox)
+
+    def mouseMoveEvent(self, event):
+        x = self.x()
+        y = self.y()
+
+        text = f"X: {x}, Y: {y}"
+
+        self.label.setText(text)
+        self.update()
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
