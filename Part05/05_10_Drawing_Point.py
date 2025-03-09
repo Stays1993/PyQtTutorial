8fc366c44e299a9ac632180044209a92b339acf4
gocac <2548171533@qq.com>
2025-03-09 09:38:26 +0800

    05_10: 鼠标单击绘制点

---
+++ Part05/05_10_Drawing_Point.py
@@ -0,0 +1,41 @@
+from PyQt6.QtGui import QIcon, QFont, QPainter, QPen
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import Qt
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(200, 200, 700, 500)
+        self.setWindowTitle("Python 05_10_Drawing_Point")
+        self.setWindowIcon(QIcon("images/python.png"))
+
+        self.pos_1 = [0, 0]
+        self.setMouseTracking(True)
+
+    """通过单击绘制点"""
+
+    def paintEvent(self, event):
+        painter: QPainter = QPainter()
+        painter.begin(self)
+
+        pen: QPen = QPen(Qt.GlobalColor.red, 15)
+        painter.setPen(pen)
+
+        painter.drawPoint(self.pos_1[0], self.pos_1[1])
+
+        painter.end()
+
+    def mousePressEvent(self, event):
+        if event.buttons() & Qt.MouseButton.LeftButton:
+            self.pos_1[0], self.pos_1[1] = self.pos().x(), self.pos().y()
+            self.update()
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
