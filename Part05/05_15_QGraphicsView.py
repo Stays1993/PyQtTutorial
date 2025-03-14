91631ebc76cfeaf5b62eb3676adaf3705b88a2cc
Stays1993 <2548171533@qq.com>
2025-03-14 09:38:13 +0800

    05_15: 图形窗口

--- Part05/05_15_QGraphicsView.py
+++ Part05/05_15_QGraphicsView.py
@@ -1,13 +1,43 @@
-from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication, QWidget, QGraphicsView
+from PyQt6.QtGui import QIcon, QPen, QBrush
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QGraphicsView,
+    QGraphicsScene,
+    QGraphicsRectItem,
+    QGraphicsEllipseItem,
+    QGraphicsItem,
+)
 
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(200, 200, 700, 500)
+        self.setGeometry(200, 200, 700, 400)
         self.setWindowTitle("PyQt6 05_15_QGraphicsView")
         self.setWindowIcon(QIcon("images/python.png"))
+
+        scene: QGraphicsScene = QGraphicsScene()
+
+        green_brush: QBrush = QBrush(Qt.GlobalColor.green)
+        yellow_brush: QBrush = QBrush(Qt.GlobalColor.yellow)
+
+        red_pen: QPen = QPen(Qt.GlobalColor.red)
+        red_pen.setWidth(7)
+
+        ellipse: QGraphicsEllipseItem = scene.addEllipse(
+            100, 100, 200, 200, red_pen, green_brush
+        )
+        rect: QGraphicsRectItem = scene.addRect(
+            -100, -100, 200, 200, red_pen, yellow_brush
+        )
+
+        ellipse.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
+        rect.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
+
+        view: QGraphicsView = QGraphicsView(scene, self)
+        view.setGeometry(0, 0, 780, 500)
 
 
 if __name__ == "__main__":
